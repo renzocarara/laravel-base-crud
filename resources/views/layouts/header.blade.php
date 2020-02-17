d3987ca5c5ca191e997ca99b288bce8df391c3bf
Renzo Carara <renzo.carara@libero.it>
2020-02-17 17:56:37 +0100

    inserito link Home per poter tornare indietro dalla view 'show'

--- resources/views/layouts/header.blade.php
+++
@@ -1,5 +0,0 @@
-<header>
-    <nav class="navbar fixed-top navbar-light bg-light">
-        <a class="navbar-brand text-center" href="{{ route('homepage') }}">Fruttivendolo CRUD</a>
-    </nav>
-</header>
